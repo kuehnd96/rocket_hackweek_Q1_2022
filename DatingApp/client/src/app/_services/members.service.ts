d381e380c17607cc8c3ece2922022ca68442fb69
David Kuehn <kuehnd96@gmail.com>
2022-04-18 15:10:19 -0500

    104: Retrieving the list of members

--- DatingApp/client/src/app/_services/members.service.ts
+++ DatingApp/client/src/app/_services/members.service.ts
@@ -5,7 +5,7 @@ import { Member } from '../_models/members';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
 }
 
@@ -18,7 +18,7 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'user', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
   }
 
   getMember(username: string) {
