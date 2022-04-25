5f513498c77acccdaa083ec99b7f810b387643d6
David Kuehn <kuehnd96@gmail.com>
2022-04-25 16:12:08 -0500

    111: Styling the member detailed page part two

--- DatingApp/client/src/app/_modules/shared.module.ts
+++ DatingApp/client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [],
@@ -11,11 +12,13 @@ import { ToastrModule } from 'ngx-toastr';
     BsDropdownModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
-    })
+    }),
+    TabsModule.forRoot()
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }
