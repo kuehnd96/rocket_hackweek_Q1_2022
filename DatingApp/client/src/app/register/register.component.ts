d84cbb8bbf809df0dead54250f681ae0bc8061ce
David Kuehn <kuehnd96@gmail.com>
2022-06-14 15:14:05 -0500

    142: Reactive forms introduction

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,13 +11,25 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    })
   }
 
   register() {
+    console.log(this.registerForm.value);
+    /*
     this.accountService.register(this.model).subscribe(response => {
       console.log(response);
       this.cancel();
@@ -24,6 +37,7 @@ export class RegisterComponent implements OnInit {
       console.log(error);
       this.toastr.error(error.error);
     });
+    */
   }
 
   cancel() {
