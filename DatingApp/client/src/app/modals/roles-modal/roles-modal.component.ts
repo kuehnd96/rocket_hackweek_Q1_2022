8a02557c71ad0a879f2a58560cc0ff23d2bd5f36
David Kuehn <kuehnd96@gmail.com>
2022-10-24 15:17:38 -0500

    218: Editing roles in the client

--- DatingApp/client/src/app/modals/roles-modal/roles-modal.component.ts
+++ DatingApp/client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-
+import { User } from 'src/app/_models/user';
 @Component({
   selector: 'app-roles-modal',
   templateUrl: './roles-modal.component.html',
   styleUrls: ['./roles-modal.component.css']
 })
 export class RolesModalComponent implements OnInit {
-  title: string;
-  list: any[] = [];
-  closeBtnName: string;
+  @Input() updateSelectedRoles = new EventEmitter();
+  user: User;
+  roles: any[];
 
   constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit(): void {
+  }
+
+  updateRoles() {
+    this.updateSelectedRoles.emit(this.roles);
+    this.bsModalRef.hide();
   }
 
 }
